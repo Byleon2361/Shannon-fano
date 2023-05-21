a3dafde7c7767c610e80348c49e2458512a490ba
Nikita Boyvalenko <n.boywalenko@yandex.ru>
2023-05-21 16:06:47 +0700

    Fixed project

---
+++ src/libShannon/compress.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <stdint.h>
+
+typedef struct value
+{
+    int count;
+    int code;
+    char codeString[31];
+    char symbol;
+} Value;
+
+int filesize(char *fileName);
+// Commpress
+int compress(char *firstFile, char *secondFile);
+Value *createDataStruct(Value *valueArr, int *sizeValueArr, char *firstFile, char *text);
+int encode(Value *valueArr, int sizeValueArr, char *text, uint8_t *res);
+void ShannonFano(Value *low, Value *high);
+Value *midFunc(Value *low, Value *high);
+Value *nextMidFunc(Value *low, Value *high, Value *mid);
